3f99a50e8b07ba5c64183bd8c901c9239bc03ff0
nightsky737 <jseto737@gmail.com>
2025-06-05 22:39:11 -0400

    started working on js version, CORs is fighting me

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 #Main app
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import generator
 from flask_session import Session
 import redis
 import connexion
 import os
+from flask_cors import CORS
 
 app = connexion.App(__name__, specification_dir="./")
 flask_app = app.app
+CORS(flask_app, origins="http://localhost:5500")  #https://medium.com/@mterrano1/cors-in-a-flask-api-38051388f8cc
 
 flask_app.secret_key = os.urandom(12)
 app.add_api("cooltexto.yml")
@@ -26,7 +28,8 @@ def home():
 def guess():
     if request.method == 'POST':
         guess_rank=generator.guess(request.form.get("guess"))
-        return render_template("guess.html", guess=request.form.get("guess"), guess_ranking=guess_rank) 
+        return jsonify({'rank': guess_rank})
+        # return render_template("guess.html", guess=request.form.get("guess"), guess_ranking=guess_rank) 
  
 if __name__ == "__main__":
     app.run(port=8000, debug=True)   
